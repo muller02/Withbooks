6c560810546d23af00204026fefd90d739b62243
naomi <keemnayn@gmail.com>
2024-04-02 17:45:11 +0900

    WithController test remove

--- src/main/java/kr/withbooks/web/controller/WithController.java
+++ src/main/java/kr/withbooks/web/controller/WithController.java
@@ -8,23 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("with")
 public class WithController {
 
-
-
     @GetMapping("list")
     public  String list(){
-
-
 
         return "/with/list";
     }
 
     @GetMapping("reg")
     public String reg(){
-        System.out.println("테스트");
-        System.out.println("테스트");
-        System.out.println("테스트");
-        System.out.println("123ㄴ테스트");
-        System.out.println("123ㄴ테스트");
+
         return "/with/reg";
     }
 }
